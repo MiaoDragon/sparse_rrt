d3b7d7c193160e045fc615ea3952be3edba24394
MiaoDragon <innocentmyl@gmail.com>
2019-10-24 21:56:46 -0700

    try directly add the direct name

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
             'src/bvp/sqp_bvp.cpp',
             'src/python_wrapper.cpp'],
         libraries=['sco', 'utils'],
-        library_dirs=['deps/trajopt/build/lib'],
+        library_dirs=['deps/trajopt/build/lib/libsco.so','deps/trajopt/build/lib/libutils.so'],
         runtime_library_dirs=['deps/trajopt/build/lib'],
         )
     ]

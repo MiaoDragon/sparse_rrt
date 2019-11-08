0efb5c21df761677ae05e4ff0a826db9bdad4521
MiaoDragon <innocentmyl@gmail.com>
2019-11-08 15:24:36 -0800

    change stdout with PIPE

--- setup.py
+++ setup.py
@@ -56,12 +56,12 @@ class CMakeBuild(build_ext):
         cmake_list_dir = os.path.abspath(os.path.dirname(__file__))
         print('-'*10, 'Running CMake prepare', '-'*40)
         subprocess.check_call(['cmake', cmake_list_dir] + cmake_args,
-                              cwd=self.build_temp, env=env, stdout=STDOUT, stderr=STDOUT)
+                              cwd=self.build_temp, env=env, stdout=PIPE, stderr=STDOUT)
 
         print('-'*10, 'Building extensions', '-'*40)
         cmake_cmd = ['cmake', '--build', '.'] + self.build_args
         subprocess.check_call(cmake_cmd,
-                              cwd=self.build_temp, stdout=STDOUT, stderr=STDOUT)
+                              cwd=self.build_temp, stdout=PIPE, stderr=STDOUT)
         print('finished cmake build.')
         # Move from build temp to final position
         for ext in self.extensions:

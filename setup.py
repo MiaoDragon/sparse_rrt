8b88181852f1d3f74c00c64907371df4e19f2631
MiaoDragon <innocentmyl@gmail.com>
2019-11-10 14:54:38 -0800

    set C++ standard

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ class CMakeBuild(build_ext):
         cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
 
         # Assuming Makefiles
-        build_args += ['--', '-j2', '-std=c++1y']
+        build_args += ['--', '-j2']
 
         self.build_args = build_args
 

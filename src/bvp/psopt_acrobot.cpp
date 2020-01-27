d15c33311414ce3698e44c4092a946d2421e159e
MiaoDragon <innocentmyl@gmail.com>
2020-01-27 14:25:42 -0800

    var name

--- src/bvp/psopt_acrobot.cpp
+++ src/bvp/psopt_acrobot.cpp
@@ -253,7 +253,7 @@ void psopt_acrobot_t::events(adouble* e, adouble* initial_states, adouble* final
 
       if (i == 0 || i == 1)
       {
-          e[STATE_N+i] = final_state[i]-2*M_PI*ceil(floor(final_state[i]/M_PI)/2);
+          e[STATE_N+i] = final_states[i]-2*M_PI*ceil(floor(final_states[i]/M_PI)/2);
           //e[STATE_N+i] = final_states[i]-2*M_PI*floor(final_states[i]/2/M_PI);
           //if (e[STATE_N+i] > M_PI)
           //{

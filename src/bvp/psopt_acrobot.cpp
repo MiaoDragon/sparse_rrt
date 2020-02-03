e4204ca1cedfbce80dec06bb6adc3d22cdda548b
MiaoDragon <innocentmyl@gmail.com>
2020-02-03 11:27:22 -0800

    try if not optimizing time in bvp

--- src/bvp/psopt_acrobot.cpp
+++ src/bvp/psopt_acrobot.cpp
@@ -231,7 +231,8 @@ adouble psopt_acrobot_t::endpoint_cost(adouble* initial_states, adouble* final_s
     // Since we already set endpoint constraint in events, we don't need it here
     // TODO: maybe we can set one end free, but try to reduce the cost only
     // Here we use the time as endpoint cost for minimum time control
-    return tf;
+    //return tf;
+    return 0.;
 }
 
 adouble psopt_acrobot_t::integrand_cost(adouble* states, adouble* controls, adouble* parameters, adouble& time, adouble* xad,

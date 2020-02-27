8c26d9a35cfa7d10b8b09ec92d89916c890ef88c
MiaoDragon <innocentmyl@gmail.com>
2020-02-26 21:05:18 -0800

    0 print level

--- src/bvp/psopt_bvp.cpp
+++ src/bvp/psopt_bvp.cpp
@@ -615,7 +615,7 @@ void PSOPT_BVP::solve(psopt_result_t& res, const double* start, const double* go
     algorithm.nlp_tolerance = 1.e-6;  // default: 1e-6
     algorithm.ode_tolerance = 1.e-1;  // default: 1e-3
     algorithm.nlp_method = "IPOPT";
-    algorithm.print_level = 1;
+    algorithm.print_level = 0;
     algorithm.collocation_method = "linear";
     algorithm.diff_matrix = "standard";  // options: "standard", "reduced-roundoff", "central-differences"
     algorithm.nsteps_error_integration = 1;

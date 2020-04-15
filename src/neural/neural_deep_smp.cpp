8a0d133ab8fe962f11a3020dbeaff2eaab750219
MiaoDragon <innocentmyl@gmail.com>
2020-04-14 18:55:00 -0700

    Update neural_deep_smp.cpp

--- src/neural/neural_deep_smp.cpp
+++ src/neural/neural_deep_smp.cpp
@@ -300,6 +300,7 @@ void MPNetSMP::plan(planner_t& SMP, system_t* system, psopt_system_t* psopt_syst
             std::cout << "SMP.state_dimension:" << SMP.get_state_dimension() << std::endl;
             std::cout << "system address:" << system << std::endl;
             std::cout << "system->state_dim: " << system->get_state_dimension() << std::endl;
+            std::cout << "this->psopt_num_iters: " << this->psopt_num_iters << std::endl;            
 
         #endif
         SMP.step_bvp(system, psopt_system, res, state_t_ptr, next_state_ptr, this->psopt_num_iters, this->psopt_num_steps, this->psopt_step_sz,

e66273574a250f433f8fd36a3d4103b889ae7d57
MiaoDragon <innocentmyl@gmail.com>
2020-05-01 18:41:20 -0700

    increaes goal_threshold

--- src/neural/neural_deep_smp.cpp
+++ src/neural/neural_deep_smp.cpp
@@ -318,7 +318,7 @@ void MPNetSMP::plan_tree(planner_t* SMP, system_t* system, psopt_system_t* psopt
 
         std::vector<double> next_state(this->state_dim);
         double use_goal_prob = uni_distribution(generator);
-        if (use_goal_prob <= pick_goal_threshold && i >= max_iteration*0.2)
+        if (use_goal_prob <= pick_goal_threshold && i >= max_iteration*0.5)
         {
             // sample the goal instead when enough max_iteration is used
             next_state = goal_state;

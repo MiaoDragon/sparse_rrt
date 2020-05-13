1234ff2e1f5f48ff282f02fb9b9fc67968dfa20a
MiaoDragon <innocentmyl@gmail.com>
2020-05-13 01:56:15 -0700

    disable printing in sst

--- src/motion_planners/sst.cpp
+++ src/motion_planners/sst.cpp
@@ -157,7 +157,7 @@ void sst_t::step_with_sample(system_interface* system, double* sample_state, dou
 	//this->random_state(sample_state);
   // sample a bunch of controls, and choose the one with the minimum distance to the sample_state
   // remember the sample state by a temperate Variable
-  std::cout << "inside step_with_sample" << std::endl;
+  //std::cout << "inside step_with_sample" << std::endl;
 
   this->random_control(new_control);
   sst_node_t* nearest = nearest_vertex(sample_state);
@@ -205,7 +205,7 @@ void sst_t::step_with_sample(system_interface* system, double* sample_state, dou
         // if iteration is 0 and the propagation fails (didn't step at all)
         // return failure
         delete past_valid_state;
-        std::cout << "after step_with_sample, failed" << std::endl;
+        //std::cout << "after step_with_sample, failed" << std::endl;
 
         return;
     }
@@ -222,7 +222,7 @@ void sst_t::step_with_sample(system_interface* system, double* sample_state, dou
         // return success
         //std::cout << "step_with_sample return: new_time: " << new_time << std::endl;
         delete past_valid_state;
-        std::cout << "after step_with_sample" << std::endl;
+        //std::cout << "after step_with_sample" << std::endl;
 
         return;
     }
@@ -244,7 +244,7 @@ void sst_t::step_with_sample(system_interface* system, double* sample_state, dou
  }
  // return success
  delete past_valid_state;
- std::cout << "after step_with_sample" << std::endl;
+ //std::cout << "after step_with_sample" << std::endl;
  return;
 
 

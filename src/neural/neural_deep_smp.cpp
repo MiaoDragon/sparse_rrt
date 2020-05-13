d0f93881b282f23baf5c2126a829bd640a6842e8
MiaoDragon <innocentmyl@gmail.com>
2020-05-13 01:26:01 -0700

    The output is strange. Try putting torch::jit::IValue creation inside the loop

--- src/neural/neural_deep_smp.cpp
+++ src/neural/neural_deep_smp.cpp
@@ -251,12 +251,13 @@ void MPNetSMP::informer_batch(at::Tensor obs, const std::vector<double>& start_s
     //mlp_input_tensor = torch::cat({obs_enc,sg}, 1);
     //torch::Tensor mlp_input_tensor_expand = mlp_input_tensor.repeat({num_sample, 1});
 
-    // iteratively obtain a list of results
-    std::vector<torch::jit::IValue> mlp_input;
-    mlp_input.push_back(mlp_input_tensor);
 
     for (int i = 0; i < num_sample; i++)
     {
+        // iteratively obtain a list of results
+        std::vector<torch::jit::IValue> mlp_input;
+        mlp_input.push_back(mlp_input_tensor);
+
         auto mlp_output = MLP->forward(mlp_input);
         torch::Tensor res = mlp_output.toTensor().to(at::kCPU);
 
